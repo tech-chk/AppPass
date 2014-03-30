86565a49909142b12036b28021f7e6c887e084b9
tech-chk <tech@chklab.com>
2014-03-30 17:56:23 +0900

    AppPassコミット

--- app/src/main/java/com/chklab/apppass/app/MainActivity.java
+++ app/src/main/java/com/chklab/apppass/app/MainActivity.java
@@ -12,6 +12,7 @@ import android.provider.SyncStateContract;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,6 +20,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher;
--- app/src/main/java/com/chklab/apppass/app/MyPassFragment.java
+++ app/src/main/java/com/chklab/apppass/app/MyPassFragment.java
@@ -1,21 +1,42 @@
 package com.chklab.apppass.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by 010144 on 14/03/25.
  */
 public class MyPassFragment extends Fragment {
+
+    private RequestQueue mQueue;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             ViewGroup container,
             Bundle savedInstanceState) {
+
+        //ネットワークからデータを取得
+        getDate(inflater.getContext());
 
         //return inflater.inflate(R.layout.fragment1, container, false);
 
@@ -39,4 +60,35 @@ public class MyPassFragment extends Fragment {
             a++;
         }
     };
+
+    private void getDate(Context context){
+
+        //リクエストURL
+        String url = context.getString(R.string.webapi_url);
+        url += "AppPassApi";
+        url += "?userid=c6c1f6c8-1307-46e9-8e47-e77709521281";
+        mQueue = Volley.newRequestQueue(context);
+        mQueue.add(new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // JSONArrayのパース、List、Viewへの追加等
+                        try {
+                            String appPassNo = response.get("apppass_no").toString();
+                            JSONArray list = response.getJSONArray("");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // エラー処理 error.networkResponseで確認
+                        // エラー表示など
+                        int a = 0;
+                    }
+                }
+        ));
+    }
 }
